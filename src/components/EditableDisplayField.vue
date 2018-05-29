d044171d1dcf6887c36429f4e1f621048496a1be
Albert VanderMeulen <albertjvm@users.noreply.github.com>
2018-05-29 10:29:14 -0400

    Add editabledisplayfield for editing name

---
+++ src/components/EditableDisplayField.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="editable-display-field">
+    <input v-if="editMode" type="text" v-on:blur="handleSave" ref="input" :value="value" />
+    <span v-else v-on:click="handleEdit">{{value}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditableDisplayField',
+  props: ['value'],
+  data () {
+    return {
+      editMode: false
+    }
+  },
+  methods: {
+    handleEdit () {
+      this.toggleEditMode()
+    },
+    handleSave () {
+      this.$emit('input', this.$refs.input.value)
+      this.toggleEditMode()
+    },
+    toggleEditMode () {
+      this.editMode = !this.editMode
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .editable-display-field {
+    span, input {
+
+    }
+    input {
+
+    }
+  }
+</style>
--- src/components/Monster.vue
+++ src/components/Monster.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="monster">
-    {{monster.name}}
+    <EditableDisplayField v-model="monster.name" v-on:input="handleNameChange(monster.name)" />
   </div>
 </template>
 
 <script>
 import firebase from 'firebase'
+import EditableDisplayField from '@/components/EditableDisplayField'
 
 export default {
   name: 'Monster',
+  components: {
+    'EditableDisplayField': EditableDisplayField
+  },
   firebase () {
     return {
       monster: {
         source: firebase.database().ref('monsters').child(this.$route.params.id),
         asObject: true
       }
+    }
+  },
+  methods: {
+    handleNameChange (name) {
+      this.$firebaseRefs.monster.child('name').set(name)
     }
   }
 }
